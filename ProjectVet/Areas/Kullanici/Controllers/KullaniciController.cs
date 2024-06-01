f13ac28f3cf430eed4bf6103b1c71d48d426ed97
Cemal İgrek <c.igrek63@hotmail.com>
2024-06-01 12:39:34 +0300

    im trying to create new layout

---
+++ ProjectVet/Areas/Kullanici/Controllers/KullaniciController.cs
@@ -0,0 +1,51 @@
+﻿using ProjectVet.EfCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjectVet.Areas.Kullanici.Controllers
+{
+    [Area("Admin")]
+    public class KullaniciController : Controller
+    {
+        private readonly KlinikContext _context;
+        public KullaniciController(KlinikContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Kullanicilar()
+        {
+            var kullaniciList =_context.Kullanicilar.ToList();
+            ViewBag.Kullanicilar=kullaniciList;
+            return View(kullaniciList);
+            
+        }
+
+
+        // POST: /Admin/Login
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            // Kullanıcı adı ve şifre kontrolü
+            if (username == "admin" && password == "admin")
+            {
+                // Başarılı giriş durumu
+                // Burada giriş başarılı olduğunda yapılacak işlemleri ekleyebilirsiniz.
+                return RedirectToAction("Index", "Admin"); // Örnek bir yönlendirme
+            }
+            else
+            {
+                // Hatalı giriş durumu
+                ViewBag.Error = "Hatalı kullanıcı adı veya şifre";
+                return View();
+            }
+        }
+    }
+}
---
+++ ProjectVet/Areas/Kullanici/Controllers/TableController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectVet.Areas.Kullanici.Controllers
+{
+    public class TableController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
