56234cbd9fd2aa4f633029e73101f4a171b8e7d6
Cemal İgrek <c.igrek63@hotmail.com>
2024-06-05 12:11:12 +0300

    düzeltildi

--- ProjectVet/Areas/Admin/Controllers/RandevuController.cs
+++ ProjectVet/Areas/Admin/Controllers/RandevuController.cs
@@ -1,44 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ProjectVet.Areas.Admin.Handlers;
+using ProjectVet.Areas.Admin.Services;
 using System;
 using System.Threading.Tasks;
 using static ProjectVet.Areas.Admin.Handlers.Commands;
-using static ProjectVet.Areas.Admin.Handlers.Queries;
 
 namespace ProjectVet.Areas.Admin.Controllers
 {
-    [Area("Admin")]
     public class RandevuController : Controller
     {
-        private readonly RandevuCommandHandler _commandHandler;
-        private readonly RandevuQueryHandler _queryHandler;
-
-        public RandevuController(RandevuCommandHandler commandHandler, RandevuQueryHandler queryHandler)
+        private readonly IRandevuService _randevuService;
+            public RandevuController(IRandevuService randevuService)
         {
-            _commandHandler = commandHandler;
-            _queryHandler = queryHandler;
+            _randevuService = randevuService;
         }
 
         [HttpPost]
         public async Task<IActionResult> Onayla(Guid id)
         {
-            var command = new OnaylaRandevuCommand { Id = id };
-            var result = await _commandHandler.Handle(command);
+            var result = await _randevuService.OnaylaRandevu(id);
             if (!result)
                 return NotFound();
 
             return Ok();
         }
+
 
         [HttpPost]
         public async Task<IActionResult> Reddet(Guid id)
         {
-            var command = new ReddetRandevuCommand { Id = id };
-            var result = await _commandHandler.Handle(command);
+            var result = await _randevuService.ReddetRandevu(id);
             if (!result)
                 return NotFound();
 
             return Ok();
         }
+
     }
 }
